491bc6fa7786ca4a1d3e9f817599efc1941968f0
Gustavo <versace123mx@gmail.com>
2024-09-07 01:41:04 -0600

    ajustes

--- src/pages/FetchProductos.jsx
+++ src/pages/FetchProductos.jsx
@@ -10,6 +10,7 @@ import {
 import Modal from "react-bootstrap/Modal";
 import Validaciones from "../helper/Validaciones";
 import Swal from "sweetalert2";
+import { formatearFecha, formatearNumero, acortarTexto, showAlert, showAlertConfirm } from "../helper/helpers";
 export async function loader({ params }) {
   let datos = await getProductos(params.page);
   let categorias = await getCategorias();
@@ -192,7 +193,11 @@ const FetchProductos = () => {
       </h1>
       <p>
         <button className="btn btn-success" onClick={handleCrear}>
-          <i className="fas fa-plus"></i> Crear
+          Crear
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle mx-1" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+          </svg>
         </button>
       </p>
       <div className="table-responsive">
@@ -230,16 +235,25 @@ const FetchProductos = () => {
                     <td>{dato.stock}</td>
                     <td className="text-center">
                       <Link to={`/fetch/productos/fotos/${dato.id}`}>
-                        <i className="fas fa-camera"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-image" viewBox="0 0 16 16">
+                          <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+                          <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1z" />
+                        </svg>
                       </Link>
                     </td>
                     <td className="text-center">
                       <Link to="#" onClick={() => { handleEditar(dato) }}>
-                        <i className="fas fa-edit"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                          <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                        </svg>
                       </Link>
                       &nbsp;&nbsp;
                       <Link to="#" onClick={() => { handleEliminar(dato.id) }}>
-                        <i className="fas fa-trash"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                        </svg>
                       </Link>
                     </td>
                   </tr>
@@ -248,6 +262,7 @@ const FetchProductos = () => {
             </table>
             <nav aria-label="Page navigation example">
               <ul className="pagination">
+                {/*
                 <li className="page-item">
                   <Link className="page-link" to={`/fetch/productos/1`}>
                     Primera
@@ -261,6 +276,7 @@ const FetchProductos = () => {
                     Anterior
                   </Link>
                 </li>
+                */}
                 {/*paginación numérica */}
                 {[...paginas].map((x, i) => (
                   <li className="page-item" key={i}>
@@ -275,6 +291,7 @@ const FetchProductos = () => {
                   </li>
                 ))}
                 {/* fin paginación numérica */}
+                {/*
                 <li className="page-item">
                   <Link
                     className="page-link"
@@ -291,6 +308,7 @@ const FetchProductos = () => {
                     Última
                   </Link>
                 </li>
+                */}
               </ul>
             </nav>
           </>
