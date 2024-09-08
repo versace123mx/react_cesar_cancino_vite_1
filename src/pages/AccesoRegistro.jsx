3c62f0b3f751d6ed68eee7140140a5e1a8854d87
Gustavo <versace123mx@gmail.com>
2024-09-07 20:02:19 -0600

    agregando login

--- src/pages/AccesoRegistro.jsx
+++ src/pages/AccesoRegistro.jsx
@@ -1,14 +1,111 @@
-import React from 'react'
+import { Link, useActionData, Form } from "react-router-dom"
+import { expresion_correo, expresion_password, showAlert, showAlertConfirm } from "../helper/helpers"
+import Validaciones from "../helper/Validaciones"
+import { authRegistro } from "../servicios/ApiAuth";
+//aqui se extraen los datos de formulario para su validacion
+/*
+Nota en la parte de los formularios hay varias formas de enviar los datos para que
+sean procesados o validados por una funcion handle o de la forma en que lo estamos
+asiendo.
+en una funcion handle seria
+<Form onSubmit={handleEnviar}>
+</Form>
+y en la funcion handleEnviar ahi realizariamos todo el temea de validacion,
 
-export async function action({}){
-    
+En este Form que estamos asiendo no utilizamos el onSubmit y para obtener los datos y validarlos lo estamso asiendo con action
+solo que de esta forma si hay que ponerle el method="post"
+*/
+export async function action({ request }) {
+    const formData = await request.formData();
+    const datos = Object.fromEntries(formData)
+    const errores = []
+    if (Object.values(datos).includes('')) {
+        errores.push("Todos los campos son obligatorios")
+    }
+    if (!expresion_correo.test(formData.get('correo'))) {
+        errores.push('El E-Mail ingresado no es válido');
+    }
+    if (datos.password != datos.password2) {
+        errores.push('Las contraseñas ingresadas no coinciden');
+    }
+    if (!expresion_password.test(formData.get('password'))) {
+        errores.push('La contraseña debe tener al menos 1 número, una mayúscula, y un largo entre 6 y 20 caracteres.');
+    }
+    // Retornar datos si hay errores
+    if (Object.keys(errores).length) {
+        //console.log(errores);
+        return errores;
+    }
+
+    //aqui mandamos los vatos via json al api
+    let registrado = await authRegistro({ name: datos.nombre, email: datos.correo, password: datos.password });
+    console.log(registrado)
+    if (registrado.estado == 'ok') {
+        showAlert('Ok','Te has registrado exitosamente','success')
+        setInterval(() => {
+            window.location = window.location.href;
+        }, 2000);
+        return true;
+    } else {
+      showAlert('Ops','Ocurrió un error, el e-Mail indicado no está disponible','error')
+      return null
+    }
 }
 const AccesoRegistro = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const errores = useActionData()//se llama aqui, por que se esta cargando desde un actio y para tener accesso lo hacemos de esta forma
+    return (
+        <>
+            <h1>Registro</h1>
+            {errores?.length && <Validaciones errores={errores} />}
+            <Form method="post">
+                <div className="form-group my-3">
+                    <label htmlFor="nombre">Nombre</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        name="nombre"
+                        className="form-control"
+                        placeholder="Nombre"
+                    />
+                </div>
+                <div className="form-group my-3">
+                    <label htmlFor="correo">E-Mail</label>
+                    <input
+                        type="text"
+                        id="correo"
+                        name="correo"
+                        className="form-control"
+                        placeholder="E-Mail"
+                    />
+                </div>
+                <div className="form-group my-3">
+                    <label htmlFor="password">Contraseña</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        className="form-control"
+                        placeholder="Contraseña"
+                    />
+                </div>
+                <div className="form-group my-3">
+                    <label htmlFor="password2">Repetir contraseña</label>
+                    <input
+                        type="password"
+                        id="password2"
+                        name="password2"
+                        className="form-control"
+                        placeholder="Repetir contraseña"
+                    />
+                </div>
+                <hr />
+                <button className='btn btn-primary'>Enviar</button>
+            </Form>
+            <div className="my-2">
+                <Link to="/acceso/login" title="¿Ya tienes cuenta? Entra aquí">¿Ya tienes cuenta? Entra aquí</Link>
+            </div>
+        </>
+    )
 }
 
 export default AccesoRegistro
--- src/servicios/ApiAuth.jsx
+++ src/servicios/ApiAuth.jsx
@@ -1,9 +1,12 @@
-import axios from "axios";
+import axios from 'axios'
+
+const cabeceros = {
+    'content-type':'application/json',
+    //'Authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MzksImlhdCI6MTcyNTQzMzE0MCwiZXhwIjoxNzI4MDI1MTQwfQ.dh06EfLh0eFItytiuXqjqWiiebPTXz2RnLDx1_wD41Q'
+}
 
 const authRegistro = async (request) => {
-    let datos = axios.post(`${import.meta.env.VITE_API_URL}registro`, request, {
-            headers: { 'content-type': 'application/json' }
-        })
+    let datos = axios.post(`${import.meta.env.VITE_API_URL}registro`, request, {headers:cabeceros})
         .then((response) => {
             return response.data;
         }).catch((error) => {
@@ -11,7 +14,7 @@ const authRegistro = async (request) => {
         });
         return datos;
     
-} 
+}
 const authLogin = async (request) => {
     let datos = axios.post(`${import.meta.env.VITE_API_URL}login`, request, {
             headers: { 'content-type': 'application/json' }
